3dffa5d9faa3f8f549924711785e4564f3226160
Tohveli <tossu@users.noreply.github.com>
2024-02-15 13:40:18 +0200

    asd

--- planner/scripts/budget.js
+++ planner/scripts/budget.js
@@ -2,3 +2,15 @@ function deleteCategory() {
     // TODO: Varoita montako kulua katoaa.
     confirm("Haluatko varmasti poistaa tämän?");
 }
+
+function toggleCategory(e) {
+    const budgetId = e.dataset.budgetId;
+    if (budgetId == null) {
+        return;
+    }
+    const expandElement = document.querySelector("[data-budget-expand-id='1234']");
+    if (expandElement == null) {
+        return;
+    }
+    expandElement.classList.toggle("hidden");
+}
